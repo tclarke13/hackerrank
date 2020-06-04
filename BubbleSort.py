e7c9a0bff862380aef2f4cf0d6b2c71875ce8d05
Taylor Clarke <taylor.clarke13@gmail.com>
2020-06-03 19:36:12 -0500

    Successful

---
+++ BubbleSort.py
@@ -0,0 +1,27 @@
+def swap(a, i, j):
+    temp = a[i]
+    a[i] = a[j]
+    a[j] = temp
+
+def countSwaps(a):
+    l = len(a)
+    swaps = 0
+    for i in range(l):
+        for j in range(l - 1):
+            if a[j] > a[j + 1]:
+                swap(a, j, j + 1)
+                swaps += 1
+    return swaps
+
+if __name__ == '__main__':
+    infile = "Input//BubbleSort.txt"
+    with open(infile, 'r') as f:
+        n = int(f.readline())
+
+        a = list(map(int, f.readline().rstrip().split()))
+
+        swaps = countSwaps(a)
+
+        print("Array is sorted in {} swaps.".format(swaps))
+        print("First Element: {}".format(a[0]))
+        print("Last Element: {}".format(a[-1]))
