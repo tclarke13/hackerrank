a59291ae1a5789e892e174af27e59ea49e720e63
Taylor Clarke <taylor.clarke13@gmail.com>
2020-06-04 22:49:30 -0500

    Finished DP for SpecialString but runtime error

--- SpecialString.py
+++ SpecialString.py
@@ -7,18 +7,34 @@ def substrCount(n, s):
         P.append([False for j in range(n)])
 
     for i in range(n):
-        dp[0][i] = 1
-        dp[i][0] = 1
-        P[0][i] = True
-        P[i][0] = True
+        dp[i][i] = 1
+        P[i][i] = True
 
-    for i in range(1, n):
-        for j in range(1, n):
-            dp[i][j] = 1
-            P[i][j] = False
+    for gap in range(1, n):
+        for i in range(n - gap):
+            j = i + gap
 
-    
+            if gap % 2:
+                if s[j] == s[j - 1]:
+                    if gap > 1:
+                        if P[i][j - 2] and s[j - 1] == s[j - 2]:
+                            P[i][j] = True
+                    else:
+                        P[i][j] = True
+            else:
+                if P[i + 1][j - 1] and s[i] == s[j]:
+                    if gap == 2 or (gap > 2 and s[j] == s[j - 1]):
+                        P[i][j] = True
 
+            dp[i][j] = dp[i][j - 1] + dp[i + 1][j] - dp[i + 1][j - 1]
+
+            if P[i][j]:
+                dp[i][j] += 1
+
+    for i in range(n):
+        print(P[i])
+        print(dp[i])
+    return dp[0][n-1]
 
 
 if __name__ == '__main__':
