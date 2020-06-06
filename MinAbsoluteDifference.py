3cd0a416e6d83278bddb58a629419333e2b2d2b0
Taylor Clarke <taylor.clarke13@gmail.com>
2020-06-05 22:53:41 -0500

    Completed MinAbsoluteDifference but did not execute in time limits

--- MinAbsoluteDifference.py
+++ MinAbsoluteDifference.py
@@ -1,4 +1,14 @@
+
+
 def minimumAbsoluteDifference(arr):
+    smallestDiff = abs(arr[0] - arr[1])
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            diff = abs(arr[i] - arr[j])
+            if diff < smallestDiff:
+                smallestDiff = diff
+
+    return smallestDiff
 
 if __name__ == '__main__':
     with open("Input//Test.txt", "r") as f:
