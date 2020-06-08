6fd80ceea1c5aa24af7aea4e80215b593e17d3ee
Taylor Clarke <taylor.clarke13@gmail.com>
2020-06-08 17:22:47 -0500

    Finished GreedyFlorist

--- GreedyFlorist.py
+++ GreedyFlorist.py
@@ -1,17 +1,23 @@
-def getMinimumCost(k, c):
+def getMinimumCost(c, k):
+    c.sort(reverse=True)
+    cost = 0
+    for i in range(len(c)):
+        cost += c[i] * (1 + i // k)
+
+    return cost
 
 
 
 if __name__ == '__main__':
     with open("Input//Test.txt", "r") as f:
-        nk = input().split()
+        nk = f.readline().rstrip().split()
 
         n = int(nk[0])
 
         k = int(nk[1])
 
-        c = list(map(int, input().rstrip().split()))
+        c = list(map(int, f.readline().rstrip().split()))
 
-        minimumCost = getMinimumCost(k, c)
+        minimumCost = getMinimumCost(c, k)
 
         print(str(minimumCost) + "\n")
