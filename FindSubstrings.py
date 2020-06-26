e7144cf92a3fbb3afd3d799d2a41d5450d0ad200
Taylor Clarke <taylor.clarke13@gmail.com>
2020-06-26 14:53:49 -0500

    FindSubstrings calculates all possible ways of splitting a string into substrings given a dictionary of substrings

---
+++ FindSubstrings.py
@@ -0,0 +1,31 @@
+def find_substrings(S, D):
+    dict = set()
+    for word in D:
+        dict.add(word)
+
+    return find_substrings_recursive(S, dict)
+
+def find_substrings_recursive(S, dict):
+    if len(S) == 1:
+        if S in dict:
+            return set(S)
+        else:
+            return set()
+    else:
+        result = set()
+        if S in dict:
+            result.add(S)
+        for i in range(1, len(S)):
+            left_substrings = find_substrings_recursive(S[0:i],dict)
+            right_substrings = find_substrings_recursive(S[i:len(S)],dict)
+            if len(left_substrings) > 0 and len(right_substrings) > 0:
+                for l in left_substrings:
+                    for r in right_substrings:
+                        result.add(l + " " + r)
+        return list(result)
+
+
+if __name__ == '__main__':
+    S = "abcd"
+    D = ["a", "b", "bc", "cd", "d", "c", "abcd", "abc", "ab"]
+    print(find_substrings(S,D))
